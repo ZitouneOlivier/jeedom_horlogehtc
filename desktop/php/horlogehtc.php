ea38d1f1dd788e1c8f6482f08b5809773dfcb6f4
ZitouneOlivier <osegura@appolonia.fr>
2017-09-15 10:25:38 +0200

    mise en page

--- desktop/php/horlogehtc.php
+++ desktop/php/horlogehtc.php
@@ -39,6 +39,8 @@ $eqLogics = eqLogic::byType('horlogehtc');
 				</center>
 				<span style="font-size : 1.1em;position:relative; top : 15px;word-break: break-all;white-space: pre-wrap;word-wrap: break-word;color:#767676"><center>{{Configuration}}</center></span>
 			</div>
+		</div>
+	</div>
 		<?php
 			foreach ($eqLogics as $eqLogic) {
 				$opacity = ($eqLogic->getIsEnable()) ? '' : jeedom::getConfiguration('eqLogic:style:noactive');
@@ -50,179 +52,153 @@ $eqLogics = eqLogic::byType('horlogehtc');
 				echo '</div>';
 			}
 		?>
-		</div>
-	</div>
 </div>
 
 
-  <div class="col-lg-10 col-md-9 col-sm-8 eqLogic" style="border-left: solid 1px #EEE; padding-left: 25px;display: none;">
-
-    <a class="btn btn-success eqLogicAction pull-right" data-action="save"><i class="fa fa-check-circle"></i> {{Sauvegarder}}</a>
-    <a class="btn btn-danger eqLogicAction pull-right" data-action="remove"><i class="fa fa-minus-circle"></i> {{Supprimer}}</a>
-
-			    <ul class="nav nav-tabs" role="tablist">
-					<li role="presentation" class="active"><a href="#eqlogictab" aria-controls="home" role="tab" data-toggle="tab"><i class="fa fa-tachometer"></i> {{Equipement}}</a></li>
-					<li role="presentation"><a href="#commandtab" aria-controls="profile" role="tab" data-toggle="tab"><i class="fa fa-list-alt"></i> {{Commandes}}</a></li>
-				</ul>
-    <div class="tab-content" style="height:calc(100% - 50px);overflow:auto;overflow-x: hidden;">
-      <div role="tabpanel" class="tab-pane active" id="eqlogictab">
-        <form class="form-horizontal">
-          <fieldset>
-            <legend>
-				<i class="fa fa-arrow-circle-left eqLogicAction cursor" data-action="returnToThumbnailDisplay"></i>  {{Général}}
-				<i class='fa fa-cogs eqLogicAction pull-right cursor expertModeVisible' data-action='configure'></i>
-            </legend>
-
-            <div class="form-group">
-              <label class="col-md-3 control-label">{{Nom de l'horloge}}</label>
-              <div class="col-md-4">
-                <input type="text" class="eqLogicAttr form-control" data-l1key="id" style="display : none;" />
-                <input type="text" class="eqLogicAttr form-control" data-l1key="name" placeholder="{{Nom de l'horloge}}"/>
-              </div>
-            </div>
-
-            <div class="form-group">
-              <label class="col-md-3 control-label" >{{Objet parent}}</label>
-              <div class="col-md-4">
-                <select class="form-control eqLogicAttr" data-l1key="object_id">
-                  <option value="">{{Aucun}}</option>
-                  <?php
-                  foreach (object::all() as $object) {
-                    echo '<option value="' . $object->getId() . '">' . $object->getName() . '</option>';
-                  }
-                  ?>
-                </select>
-              </div>
-            </div>
-
-            <div class="form-group">
-              <label class="col-md-3 control-label">{{Catégorie}}</label>
-              <div class="col-md-6">
-                <?php
-                foreach (jeedom::getConfiguration('eqLogic:category') as $key => $value) {
-                  echo '<label class="checkbox-inline">';
-                  echo '<input type="checkbox" class="eqLogicAttr" data-l1key="category" data-l2key="' . $key . '" />' . $value['name'];
-                  echo '</label>';
-                }
-                ?>
-
-              </div>
-            </div>
-
-            <div class="form-group">
-			  <label class="col-md-3 control-label" ></label>
-              <div class="col-md-6">
-                <label class="checkbox-inline"><input type="checkbox" class="eqLogicAttr" data-l1key="isEnable" checked/>{{Activer}}</label>
-                <label class="checkbox-inline"><input type="checkbox" class="eqLogicAttr" data-l1key="isVisible" checked/>{{Visible}}</label>
-              </div>
-			</div>
-
-            <div class="form-group">
-              <label class="col-sm-3 control-label">{{Commentaire}}</label>
-              <div class="col-md-4">
-                <textarea class="eqLogicAttr form-control" data-l1key="configuration" data-l2key="commentaire" ></textarea>
-              </div>
-            </div>
-
-          </fieldset>
-
-        </form>
-
-        <form class="form-horizontal">
-          <fieldset>
-            <legend><i class="fa fa-info-circle"></i>  {{Configuration Météo}}</legend>
-
-			<div class="form-group">
-				<label class="col-md-3 control-label">{{Heure de collecte}}</label>
+<div class="col-lg-10 col-md-9 col-sm-8 eqLogic" style="border-left: solid 1px #EEE; padding-left: 25px;display: none;">
+	<a class="btn btn-success eqLogicAction pull-right" data-action="save"><i class="fa fa-check-circle"></i> {{Sauvegarder}}</a>
+	<a class="btn btn-danger eqLogicAction pull-right" data-action="remove"><i class="fa fa-minus-circle"></i> {{Supprimer}}</a>
+	<ul class="nav nav-tabs" role="tablist">
+		<li role="presentation" class="active"><a href="#eqlogictab" aria-controls="home" role="tab" data-toggle="tab"><i class="fa fa-tachometer"></i> {{Equipement}}</a></li>
+		<li role="presentation"><a href="#commandtab" aria-controls="profile" role="tab" data-toggle="tab"><i class="fa fa-list-alt"></i> {{Commandes}}</a></li>
+	</ul>
+	<div class="tab-content" style="height:calc(100% - 50px);overflow:auto;overflow-x: hidden;">
+		<div role="tabpanel" class="tab-pane active" id="eqlogictab">
+			<form class="form-horizontal">
+				<fieldset>
+				<legend>
+					<i class="fa fa-arrow-circle-left eqLogicAction cursor" data-action="returnToThumbnailDisplay"></i>  {{Général}}
+					<i class='fa fa-cogs eqLogicAction pull-right cursor expertModeVisible' data-action='configure'></i>
+				</legend>
+				<div class="form-group">
+					<label class="col-md-3 control-label">{{Nom de l'horloge}}</label>
 					<div class="col-md-4">
-					    <label class="checkbox-inline"><input type="checkbox" class="eqLogicAttr" data-l1key="configuration" data-l2key="CollectDateIsVisible" checked/>{{Afficher}}</label>
+						<input type="text" class="eqLogicAttr form-control" data-l1key="id" style="display : none;" />
+						<input type="text" class="eqLogicAttr form-control" data-l1key="name" placeholder="{{Nom de l'horloge}}"/>
 					</div>
-			</div>
-
-			<div class="form-group">
-				<label class="col-md-3 control-label">{{Météo}}</label>
+				</div>
+				<div class="form-group">
+					<label class="col-md-3 control-label" >{{Objet parent}}</label>
 					<div class="col-md-4">
-						<label class="checkbox-inline"><input type="checkbox" class="eqLogicAttr" data-l1key="configuration" data-l2key="MeteoOn" checked/>{{Afficher}}</label>
-					</div>
-			</div>
-
-
-            <div class="form-group">
-              <label class="col-md-3 control-label">{{Coordonnées GPS}}</label>
-              <div class="col-md-4">
-                <input type="text" class="eqLogicAttr configuration form-control" data-l1key="configuration" data-l2key="coordonees"/>
-              </div>
-            </div>
-
-            <div class="form-group">
-              <label class="col-md-3 control-label">{{Clef API Forecast.io}}</label>
-              <div class="col-md-4">
-                <input type="text" class="eqLogicAttr configuration form-control" data-l1key="configuration" data-l2key="apikey"/>
-              </div>
-            </div>
-
-           	<div class="form-group">
-			<label class="col-md-3 control-label">{{Température Local}}</label>
-				<div class="col-md-4">
-					<div class="input-group">
-						<input type="text"  class="eqLogicAttr configuration form-control" data-l1key="configuration" data-l2key="temperaturelocal" />
-						<span class="input-group-btn">
-							<a class="btn btn-default cursor" title="Rechercher une commande" id="bt_selectTemperature"><i class="fa fa-list-alt"></i></a>
-						</span>
+						<select class="form-control eqLogicAttr" data-l1key="object_id">
+							<option value="">{{Aucun}}</option>
+							<?php
+								foreach (object::all() as $object) {
+									echo '<option value="' . $object->getId() . '">' . $object->getName() . '</option>';
+								}
+							?>
+						</select>
 					</div>
 				</div>
-			<label class="checkbox-inline"><input type="checkbox" class="eqLogicAttr" data-l1key="configuration" data-l2key="TemperatureIsLocal" checked/>{{Utiliser}}</label>
-			</div>
-			
-			<div class="form-group">
-			<label class="col-md-3 control-label">{{Pression Local}}</label>
-				<div class="col-md-4">
-					<div class="input-group">
-						<input type="text"  class="eqLogicAttr configuration form-control" data-l1key="configuration" data-l2key="pressionlocal" />
-						<span class="input-group-btn">
-							<a class="btn btn-default cursor" title="Rechercher une commande" id="bt_selectPression"><i class="fa fa-list-alt"></i></a>
-						</span>
-					</div>
+				<div class="form-group">
+					<label class="col-md-3 control-label">{{Catégorie}}</label>
+						<div class="col-md-6">
+							<?php
+								foreach (jeedom::getConfiguration('eqLogic:category') as $key => $value) {
+									echo '<label class="checkbox-inline">';
+									echo '<input type="checkbox" class="eqLogicAttr" data-l1key="category" data-l2key="' . $key . '" />' . $value['name'];
+									echo '</label>';
+								}
+							?>
+						</div>
 				</div>
-			<label class="checkbox-inline"><input type="checkbox" class="eqLogicAttr" data-l1key="configuration" data-l2key="PressionIsLocal" checked/>{{Utiliser}}</label>
-			</div>
-			
-			<div class="form-group">
-			<label class="col-md-3 control-label">{{Humidite Local}}</label>
-				<div class="col-md-4">
-					<div class="input-group">
-						<input type="text"  class="eqLogicAttr configuration form-control" data-l1key="configuration" data-l2key="humiditelocal" />
-						<span class="input-group-btn">
-							<a class="btn btn-default cursor" title="Rechercher une commande" id="bt_selectHumidite"><i class="fa fa-list-alt"></i></a>
-						</span>
-					</div>
+				<div class="form-group">
+					<label class="col-md-3 control-label" ></label>
+						<div class="col-md-6">
+							<label class="checkbox-inline"><input type="checkbox" class="eqLogicAttr" data-l1key="isEnable" checked/>{{Activer}}</label>
+							<label class="checkbox-inline"><input type="checkbox" class="eqLogicAttr" data-l1key="isVisible" checked/>{{Visible}}</label>
+						</div>
 				</div>
-			<label class="checkbox-inline"><input type="checkbox" class="eqLogicAttr" data-l1key="configuration" data-l2key="HumiditeIsLocal" checked/>{{Utiliser}}</label>
-			</div>
-
-
-          </fieldset>
-        </form>
-      </div>
-
-
-	<div role="tabpanel" class="tab-pane" id="commandtab">
-		<legend><i class="fa fa-cloud"></i>  {{Valeurs Actuelles}}</legend>
-		<table id="table_cmd" class="table table-bordered table-condensed">
-			<thead>
-				<tr>
-					<th>{{Origine}}</th>
-					<th>{{Nom}}</th>
-					<th>{{Valeur}}</th>
-					<th>{{Paramètres}}</th>
-				 </tr>
-			</thead>
-			<tbody>
-			</tbody>
-		</table>
+				<div class="form-group">
+					<label class="col-sm-3 control-label">{{Commentaire}}</label>
+						<div class="col-md-4">
+							<textarea class="eqLogicAttr form-control" data-l1key="configuration" data-l2key="commentaire" ></textarea>
+						</div>
+				</div>
+				</fieldset>
+			</form>
+			<form class="form-horizontal">
+				<fieldset>
+					<legend><i class="fa fa-info-circle"></i>  {{Configuration Météo}}</legend>
+					<div class="form-group">
+						<label class="col-md-3 control-label">{{Heure de collecte}}</label>
+						<div class="col-md-4">
+							<label class="checkbox-inline"><input type="checkbox" class="eqLogicAttr" data-l1key="configuration" data-l2key="CollectDateIsVisible" checked/>{{Afficher}}</label>
+						</div>
+					</div>
+					<div class="form-group">
+						<label class="col-md-3 control-label">{{Météo}}</label>
+						<div class="col-md-4">
+							<label class="checkbox-inline"><input type="checkbox" class="eqLogicAttr" data-l1key="configuration" data-l2key="MeteoOn" checked/>{{Afficher}}</label>
+						</div>
+					</div>
+					<div class="form-group">
+						<label class="col-md-3 control-label">{{Coordonnées GPS}}</label>
+						<div class="col-md-4">
+							<input type="text" class="eqLogicAttr configuration form-control" data-l1key="configuration" data-l2key="coordonees"/>
+						</div>
+					</div>
+					<div class="form-group">
+						<label class="col-md-3 control-label">{{Clef API Forecast.io}}</label>
+						<div class="col-md-4">
+							<input type="text" class="eqLogicAttr configuration form-control" data-l1key="configuration" data-l2key="apikey"/>
+						</div>
+					</div>
+					<div class="form-group">
+						<label class="col-md-3 control-label">{{Température Local}}</label>
+							<div class="col-md-4">
+								<div class="input-group">
+									<input type="text"  class="eqLogicAttr configuration form-control" data-l1key="configuration" data-l2key="temperaturelocal" />
+									<span class="input-group-btn">
+										<a class="btn btn-default cursor" title="Rechercher une commande" id="bt_selectTemperature"><i class="fa fa-list-alt"></i></a>
+									</span>
+								</div>
+							</div>
+						<label class="checkbox-inline"><input type="checkbox" class="eqLogicAttr" data-l1key="configuration" data-l2key="TemperatureIsLocal" checked/>{{Utiliser}}</label>
+					</div>
+					<div class="form-group">
+						<label class="col-md-3 control-label">{{Pression Local}}</label>
+						<div class="col-md-4">
+							<div class="input-group">
+								<input type="text"  class="eqLogicAttr configuration form-control" data-l1key="configuration" data-l2key="pressionlocal" />
+								<span class="input-group-btn">
+									<a class="btn btn-default cursor" title="Rechercher une commande" id="bt_selectPression"><i class="fa fa-list-alt"></i></a>
+								</span>
+							</div>
+						</div>
+						<label class="checkbox-inline"><input type="checkbox" class="eqLogicAttr" data-l1key="configuration" data-l2key="PressionIsLocal" checked/>{{Utiliser}}</label>
+					</div>
+					<div class="form-group">
+						<label class="col-md-3 control-label">{{Humidite Local}}</label>
+						<div class="col-md-4">
+							<div class="input-group">
+								<input type="text"  class="eqLogicAttr configuration form-control" data-l1key="configuration" data-l2key="humiditelocal" />
+									<span class="input-group-btn">
+										<a class="btn btn-default cursor" title="Rechercher une commande" id="bt_selectHumidite"><i class="fa fa-list-alt"></i></a>
+									</span>
+							</div>
+						</div>
+						<label class="checkbox-inline"><input type="checkbox" class="eqLogicAttr" data-l1key="configuration" data-l2key="HumiditeIsLocal" checked/>{{Utiliser}}</label>
+					</div>
+				</fieldset>
+			</form>
+		</div>
+		<div role="tabpanel" class="tab-pane" id="commandtab">
+			<legend><i class="fa fa-cloud"></i>  {{Valeurs Actuelles}}</legend>
+			<table id="table_cmd" class="table table-bordered table-condensed">
+				<thead>
+					<tr>
+						<th>{{Origine}}</th>
+						<th>{{Nom}}</th>
+						<th>{{Valeur}}</th>
+						<th>{{Paramètres}}</th>
+					 </tr>
+				</thead>
+				<tbody>
+				</tbody>
+			</table>
+		</div>
 	</div>
-
-  </div>
 </div>
 
 <?php include_file('desktop', 'horlogehtc', 'js', 'horlogehtc'); ?>
